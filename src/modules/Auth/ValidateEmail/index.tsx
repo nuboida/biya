0fdbcaffd6ddc46e39d3f390578a271ce8ca7ae2
Idowu Fadase <idowu.fadase@ollasystems.com>
2024-05-16 12:37:00 +0100

    add stop image

--- src/modules/Auth/ValidateEmail/index.tsx
+++ src/modules/Auth/ValidateEmail/index.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React from "react";
+import Image from "next/image";
 import { BiyaIcon } from "@/components/Icon";
 
 
@@ -12,7 +13,7 @@ const ValidateEmail = () => {
   return (
     <div className="lg:p-10 2xl:p-28 max-lg:px-5">
       <div className="flex flex-col justify-center items-center gap-10">
-        <div className="">
+        <div>
           <BiyaIcon name="mail"/>
         </div>
         <div className="flex flex-col justify-center items-center gap-1">
@@ -20,9 +21,15 @@ const ValidateEmail = () => {
           <p>Check your Email and click the link to activate.</p>
         </div>
       </div>
-      <div className="mt-10 flex items-center">
-        <span><BiyaIcon name="info" /></span>
-        <p>Thank you for your interest. We are current in beta and if you’ll like to be a part of it, please send an email to us on <span className="text-blue">info@biya.com.ng</span> </p>
+      <div className="flex flex-col items-center">
+        <div>
+        <Image
+        src={"/images/stopimage.svg"}
+        alt="stop image"
+        width={400}
+        height={400}
+        /></div>
+        <p className="text-rose-500">Thank you for your interest. We are currently in beta and if you’ll like to be a part of it, please send an email to us on <span className="text-blue">info@biya.com.ng</span> </p>
       </div>
     </div>
   )
