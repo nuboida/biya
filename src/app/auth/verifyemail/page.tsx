71c148e74d5b13654904c0583b252c63ed75f335
Idowu Fadase <idowu.fadase@ollasystems.com>
2024-12-29 07:17:15 +0100

    add key condition

--- src/app/auth/verifyemail/page.tsx
+++ src/app/auth/verifyemail/page.tsx
@@ -3,13 +3,21 @@
 import React from "react";
 import AuthLayout from "@/modules/layouts/AuthLayout/AuthLayout";
 import EmailVerified from "@/modules/Auth/EmailVerified";
+import { useParams, useRouter } from "next/navigation";
 
 const VerifyEmailPage = () => {
-  return (
-    <AuthLayout>
-      <EmailVerified />
-    </AuthLayout>
-  )
+  const params = useParams();
+  const router = useRouter();
+
+  if (params.key != '') {
+    return (
+      <AuthLayout>
+        <EmailVerified />
+      </AuthLayout>
+    )
+  } else {
+    router.push("/auth/signup");
+  }
 }
 
 export default VerifyEmailPage;
